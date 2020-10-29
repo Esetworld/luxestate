a5002c5df328555d79c68cb2c449ba059c6af2af
Esetworld <73596111+Esetworld@users.noreply.github.com>
2020-10-29 15:39:48 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+let stpbl1 = document.getElementById("stpbl1")
+let stpbrdr1 = document.getElementById("tpbrdr1")
+
+let stpbl2 = document.getElementById("stpbl2")
+let stpbrdr2 = document.getElementById("tpbrdr2")
+
+let stpbl3 = document.getElementById("stpbl3")
+let stpbrdr3 = document.getElementById("tpbrdr3")
+
+stpbl1.onmouseover = function() {
+    stpbrdr1.style.borderBottom = "3px solid white"
+}
+stpbl1.onmouseout = function() {
+    stpbrdr1.style.borderBottom = "3px solid black"
+}
+
+stpbl2.onmouseover = function() {
+    stpbrdr1.style.borderBottom = "3px solid white"
+}
+stpbl2.onmouseout = function() {
+    stpbrdr1.style.borderBottom = "3px solid black"
+}
+
+stpbl3.onmouseover = function() {
+    stpbrdr1.style.borderBottom = "3px solid white"
+}
+stpbl3.onmouseout = function() {
+    stpbrdr1.style.borderBottom = "3px solid black"
+}
